33fee571eb88ad21ba6cc02ffdf4f30af254a233
Abhimanyu <abhirathore2006@gmail.com>
2019-10-25 01:01:59 +0530

    handle multiple scripts

--- src/components/Script.js
+++ src/components/Script.js
@@ -2,21 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export class HTMLScript extends React.Component {
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  componentDidMount() {}
+  componentDidUpdate() {
     let { content } = this.props;
-    let scriptContent = content || '';
-    //html parsing is never safe, hence decided to use replace and then split to fetch all content
-    scriptContent = scriptContent.replace(/<script>/g, '');
-    let allScripts = scriptContent.split('</script>');
-    let rootEle = document.getElementById(`script-holder-${this.props.holderId}`);
-    allScripts.forEach(s => {
-      let scriptEle = document.createElement('script');
-      scriptEle.innerHTML = s;
-      rootEle.append(scriptEle);
-    });
+    setTimeout(() => {
+      let el = document.createElement('html');
+      el.innerHTML = content;
+      let scriptTags = el.getElementsByTagName('script');
+      console.log(scriptTags);
+      for (let i = 0; i < scriptTags.length; i++) {
+        let s = scriptTags[i];
+        let scriptEle = document.createElement('script');
+        if (s.attributes && s.attributes.src) {
+          Array.from(s.attributes).forEach(attr => {
+            scriptEle[attr.name] = attr.value;
+          });
+          this.myRef.current.appendChild(scriptEle);
+        } else {
+          scriptEle.innerHTML = s.innerHTML;
+          this.myRef.current.appendChild(scriptEle);
+        }
+      }
+      // if (content) {
+      //   let scriptContent = content || '';
+      //   // html parsing is never safe, hence decided to use replace and then split to fetch all content
+      //   scriptContent = scriptContent.replace(/<script>/g, '');
+      //   let allScripts = scriptContent.split('</script>');
+
+      //   allScripts.forEach(s => {
+      //     let scriptEle = document.createElement('script');
+      //     scriptEle.innerHTML = s;
+      //     this.myRef.current.appendChild(scriptEle);
+      //   });
+      // }
+    }, 1000);
   }
   render() {
-    return <div id={`script-holder-${this.props.holderId}`} />;
+    return <div ref={this.myRef} />;
   }
 }
 
